f065710d57531e8833d9cf21ee2d8f52e0295b64
Indra Bijay <indrabijay007@gmail.com>
2016-04-23 18:20:17 -0700

    corrected the javascript issue with type

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-/**
- * Created by indrabijaynarayan on 4/22/16.
- */
-var app=angular.module("myApp",[]);
-app.controller("myCtrl",["$scope",function ($scope) {
-
+angular.module('myApp',[])
+.controller('myCtrl',['$scope',function($scope){
+   
 }]);
