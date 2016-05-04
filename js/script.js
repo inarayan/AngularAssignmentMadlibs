ea9268414c1828b73e0d50f20a5c1480d58e9b6e
Indra Bijay <indrabijay007@gmail.com>
2016-05-03 22:25:57 -0700

    add angular form to madlib game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-angular.module('myApp',[])
+angular.module('myApp',['ngMessages'])
 .controller('myCtrl',['$scope',function($scope){
 
-   $scope.gender='female';
+    $scope.gender='female';
+    $scope.reset=true;
 
+  $scope.submit=function()
+  {
+      $scope.madlibform.$submitted=true;
+      $scope.reset=false;
+      
+  };
+    $scope.resetfunc=function()
+   {
+       $scope.madlibform.$submitted=false;
+       $scope.reset=true;
+   }
 }]);
