dfe729828bf069a0c49a23fd889037df74937b5f
Indra Bijay <indrabijay007@gmail.com>
2016-05-04 12:30:03 -0700

    Add the logic to hide and show div as per the users button press

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 angular.module('myApp',['ngMessages'])
-.controller('myCtrl',['$scope',function($scope){
+.controller('myCtrl',['$scope', function($scope){
 
     $scope.gender='female';
     $scope.reset=true;
 
-  $scope.submit=function()
-  {
+
+    $scope.submit=function()
+    {
+    
       $scope.madlibform.$submitted=true;
       $scope.reset=false;
-      
-  };
+
+
+    };
+
     $scope.resetfunc=function()
    {
        $scope.madlibform.$submitted=false;
        $scope.reset=true;
-   }
+
+       $scope.female_name=undefined;
+       $scope.dirty_task=undefined;
+       $scope.obnoxious_celebrity=undefined;
+
+       $scope.celebrity=undefined;
+       $scope.job_title=undefined;
+
+       $scope.huge_number=undefined;
+       $scope.useless_skill=undefined;
+       $scope.tedious_task=undefined;
+       $scope.adjective=undefined;
+
+   };
+
+
 }]);
